50d7f6d2b7399cc549c2936bf9b519036fdd5210
szycic <szymoncichy175@gmail.com>
2025-06-27 16:34:49 +0000

    Deploying to docs from @ szycic/sc-faktury@4f84a72d457acf780e54cb30e641e448d9a5760c 🚀

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.szycic.invoice","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"com.szycic.invoice","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.szycic.invoice","l":"Main"}];updateSearchResults();
